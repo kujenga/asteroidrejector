c414d4ff22e446b8d905e6b1d07e70cecfc12b94
kujenga <ataylor0123@gmail.com>
2014-11-23 21:15:16 +0100

    added example data input files

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 # a script to automatically run the test program
 
-DATA_FOLDER='offline_training'
-TRAIN_FILE=''
-TEST_FILE=''
+# customizable arguments
+DATA_FOLDER='./offline_training/'
+TRAIN_FILE='./example_data/example_train.txt'
+TEST_FILE='./example_data/example_test.txt'
 
-EXEC_COMMAND='python asteroid_rejector.py'
+EXEC_COMMAND='python ./asteroid_rejector.py'
 
+# command supplied by the competition
 java -jar tester.jar -folder $DATA_FOLDER -train $TRAIN_FILE -test $TEST_FILE -exec $EXEC_COMMAND -vis
