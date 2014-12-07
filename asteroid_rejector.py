b01981f7c556c70510850a1c186fb803fa157757
kujenga <ataylor0123@gmail.com>
2014-12-07 19:52:33 +0100

    refactored some of the tester, added np.array creation for detections

--- asteroid_rejector.py
+++ asteroid_rejector.py
@@ -3,11 +3,7 @@
 
 # Aaron Taylor
 
-from sys import stdin
-from sys import stdout
 from sys import stderr
-
-import pdb
 
 import numpy as np
 from io import StringIO   # StringIO behaves like a file object
@@ -16,17 +12,18 @@ from io import StringIO   # StringIO behaves like a file object
 class AsteroidRejector:
 
     def __init__(self):
-        stderr.write("init is currently empty\n")
+        self.det_list_dtypes = {"names": ("det_num", "frame_num", "sexnum", "time", "ra", "dec", "x", "y", "magnitude", "fwhm", "elong", "theta", "rmse", "deltamu", "rejected"),
+                                "formats": (int, int, int, float, float, float, float, float, float, float, float, float, float, float, float, int)}
 
     # Class:	AsteroidRejector
     # Method:	trainingData
     # Parameters:	int[], String[]
     # Returns:	int
     # Method signature:	int trainingData(int[] imageData, String[] detections)
-    def training_data(self, imageData, detections):
-        # stderr.write(str(detections)+"\n")
-        stderr.write(str(type(detections))+"\n")
-        stderr.write("training_data not yet implemented\n")
+    def training_data(self, imageData, detections_array):
+        detections = np.array(detections_array)
+        print(type(detections))
+        print (detections[0])
         return 0
 
     # Method:	testingData
@@ -46,48 +43,48 @@ class AsteroidRejector:
         return []
 
 
-if __name__ == "__main__":
-    astRejector = AsteroidRejector()
-
-    det_list_dtypes = {"names": ("det_num", "frame_num", "sexnum", "time", "ra", "dec", "x", "y", "magnitude", "fwhm", "elong", "theta", "rmse", "deltamu", "rejected"),
-                       "formats": (int, int, int, float, float, float, float, float, float, float, float, float, float, float, float, int)}
-
-    for i in range(1000):
-        N = int(stdin.readline())
-        imageData = []
-        for j in range(N):
-            imageData.append(int(stdin.readline()))
-        imageData = np.array(imageData)
-
-        M = int(stdin.readline())
-        detections = []
-        for j in range(M):
-            detect_str = stdin.readline()
-            det = np.loadtxt(StringIO(detect_str), dtype=det_list_dtypes)
-            detections.append(det)
-        detections = np.array(detections)
-
-        result = astRejector.training_data(imageData, detections)
-        stdout.write(str(result))
-        stdout.flush()
-
-    for i in range(200):
-        N = int(stdin.readline())
-        imageData = []
-        for j in range(N):
-            imageData.append(int(stdin.readline()))
-        imageData = np.array(imageData)
-
-        M = int(stdin.readline())
-        detect_str = stdin.readline()
-        detections = np.loadtxt(StringIO(detect_str), dtype=det_list_dtypes)
-
-        result = astRejector.testing_data(imageData, detections)
-        stdout.write(result)
-        stdout.flush()
-
-    results = astRejector.get_answer
-    stdout.write(len(results))
-    for i in range(len(results)):
-        stdout.write(results[i])
-    stdout.flush()
+# if __name__ == "__main__":
+#     astRejector = AsteroidRejector()
+#
+#     det_list_dtypes = {"names": ("det_num", "frame_num", "sexnum", "time", "ra", "dec", "x", "y", "magnitude", "fwhm", "elong", "theta", "rmse", "deltamu", "rejected"),
+#                        "formats": (int, int, int, float, float, float, float, float, float, float, float, float, float, float, float, int)}
+#
+#     for i in range(1000):
+#         N = int(stdin.readline())
+#         imageData = []
+#         for j in range(N):
+#             imageData.append(int(stdin.readline()))
+#         imageData = np.array(imageData)
+#
+#         M = int(stdin.readline())
+#         detections = []
+#         for j in range(M):
+#             detect_str = stdin.readline()
+#             det = np.loadtxt(StringIO(detect_str), dtype=det_list_dtypes)
+#             detections.append(det)
+#         detections = np.array(detections)
+#
+#         result = astRejector.training_data(imageData, detections)
+#         stdout.write(str(result))
+#         stdout.flush()
+#
+#     for i in range(200):
+#         N = int(stdin.readline())
+#         imageData = []
+#         for j in range(N):
+#             imageData.append(int(stdin.readline()))
+#         imageData = np.array(imageData)
+#
+#         M = int(stdin.readline())
+#         detect_str = stdin.readline()
+#         detections = np.loadtxt(StringIO(detect_str), dtype=det_list_dtypes)
+#
+#         result = astRejector.testing_data(imageData, detections)
+#         stdout.write(result)
+#         stdout.flush()
+#
+#     results = astRejector.get_answer
+#     stdout.write(len(results))
+#     for i in range(len(results)):
+#         stdout.write(results[i])
+#     stdout.flush()
--- reject_tester.py
+++ reject_tester.py
@@ -130,20 +130,7 @@ class RejectTester:
                 raw.append(v)
         print(filename + " loaded. Size = " + str(len(raw)))
 
-    def doExec(self):
-
-        # launch solution
-        self.printMessage("Executing your solution: " + self.execCommand + ".")
-
-        ast_rejector = AsteroidRejector()
-
-        # solution = Popen(execCommand.split(), stdout=PIPE, stdin=PIPE, stderr=PIPE)
-        # stdout_data = solution.communicate(input='data_to_write')[0]
-
-        # reader = new BufferedReader(InputStreamReader(solution.getInputStream()))
-        # PrintWriter writer = new PrintWriter(solution.getOutputStream())
-        # new ErrorStreamRedirector(solution.getErrorStream()).start()
-
+    def trainRejector(self, ast_rejector):
         # read training file
         self.printMessage("Training...")
         det_id = 0
@@ -181,6 +168,7 @@ class RejectTester:
             # self.printMessage(folder + s + ".det loaded. Rows = " + detTraining.size())
 
             if (self.visualize):
+                n = len(rawTraining)/(4*64*64)
                 for i in range(rawTraining.size()/(4*64*64)):
                     case_num = det_id - n + i
                     fileName = case_num + ".png"
@@ -191,23 +179,12 @@ class RejectTester:
                     visualize(rawTraining, i*4*64*64, fileName)
 
             # call trainingData(imageData, detections)
-            trainResp = ast_rejector.training_data(rawTraining, detTraining)
+            ast_rejector.training_data(rawTraining, detTraining)
 
-            # writer.println(imageData_train.length)
-            # for v in imageData_train:
-            #     writer.println(v)
-            # writer.flush()
-            #
-            # writer.println(detections_train.length)
-            # for v in detections_train:
-            #     writer.println(v)
-            # writer.flush()
-
-            # get response from solution
-            # trainResp = reader.readline()
-
+    def testRejector(self, ast_rejector):
         # read testing file
         self.printMessage("Testing...")
+        det_id = 0
         modelAnsReject = set([])
         modelAnsDetect = set([])
         for s in open(testFile):
@@ -217,14 +194,14 @@ class RejectTester:
 
             # load raw image data
             rawTest = []
-            loadRawImage(folder + s + ".raw", rawTest)
+            self.loadRawImage(folder + s + ".raw", rawTest)
             # load detection data
             detTest = []
             brdet = open(folder + s + ".det", 'w')
             cnt = 0
-            while (true):
+            while (True):
                 row = brdet.readline()
-                if (row == null):
+                if (not row):
                     break
                 row = det_id + " " + row
                 if (row.charAt(row.length()-1) == '1'):
@@ -238,10 +215,10 @@ class RejectTester:
                 cnt += 1
                 if ((cnt % 4) == 0):
                     det_id += 1
-
             brdet.close()
 
             if (visualize):
+                n = len(rawTest)/(4*64*64)
                 for i in range(rawTest.size()/(4*64*64)):
                     case_num = det_id - n + i
                     fileName = case_num + ".png"
@@ -251,27 +228,17 @@ class RejectTester:
                         fileName = "D_" + fileName
                     visualize(rawTest, i*4*64*64, fileName)
 
-            # call testData(imageData, detections)
-            imageData_test = [0]*len(rawTest)
-            for i in range(rawTest.size()):
-                imageData_test[i] = rawTest.get(i)
-            detections_test = [""]*len(detTest)
-            detTest.toArray(detections_test)
+            ast_rejector.training_data(rawTest, detTest)
 
-            writer.println(imageData_test.length)
-            for v in imageData_test:
-                writer.println(v)
+    def doExec(self):
+        self.printMessage("Executing your solution: " + self.execCommand + ".")
+        # create solution class
+        ast_rejector = AsteroidRejector()
 
-            writer.flush()
+        # train the asteroid rejector
+        self.trainRejector(ast_rejector)
 
-            writer.println(detections_test.length)
-            for v in detections_test:
-                writer.println(v)
-
-            writer.flush()
-
-            # get response from solution
-            testResp = reader.readline()
+        self.testRejector(ast_rejector)
 
         # # get response from solution
         # cmd = reader.readLine()
