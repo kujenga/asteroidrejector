da8494c891bb7387cd48bb8389fe9d66b55b5cd1
kujenga <ataylor0123@gmail.com>
2014-12-09 12:02:50 +0100

    added silly if statement to run python script on server

--- py_run.sh
+++ py_run.sh
@@ -1,9 +1,15 @@
 # a script to automatically run the test program
 
 # customizable arguments
-DATA_FOLDER='./offline_training/'
-TRAIN_FILE='./example_data/tiny_train.txt'
-TEST_FILE='./example_data/tiny_test.txt'
+if [ -d ~/Documents ]; then
+  DATA_FOLDER='./offline_training/'
+  TRAIN_FILE='./example_data/tiny_train.txt'
+  TEST_FILE='./example_data/tiny_test.txt'
+else
+  DATA_FOLDER='/mnt/idms/AIT_Aaron/offline_training/'
+  TRAIN_FILE='./example_data/tiny_train.txt'
+  TEST_FILE='./example_data/tiny_test.txt'
+fi
 
 # command supplied by the competition
 python3 reject_tester.py -folder $DATA_FOLDER -train $TRAIN_FILE -test $TEST_FILE -exec 'python3 asteroid_rejector.py' # -debug -vis
